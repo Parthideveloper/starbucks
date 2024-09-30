beef27cd503ecf22c3962017234f4c13286a6666
Parthiban <136749124+Parthideveloper@users.noreply.github.com>
2024-09-30 20:16:13 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+function imgslider(anything){
+          document.querySelector('.starbucks').src = anything;
+
+};
+
+function colorchange(color){
+     const circle = document.querySelector('.circle');
+     circle.style.background = color;
+     document.getElementById('color-change-span').style.color = color;
+
+
+}
+
+// const h2 = document.querySelector('.starcolor');
+
